2572304e34451f0086541dd3f40044d79c99301b
github username <github email address>
2021-02-19 11:57:04 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #return true if self.match(/[.]$/)
+    #false
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_self = self.split(/[.|?|!]\s+/) #the \s+ gets rid of the white spaces when splitting
+    count = split_self.length
+    if count
+      return count
+    else
+      return 0
+    end
+  
   end
 end
+
+#basically, using self is like calling a method without reieving an arg
